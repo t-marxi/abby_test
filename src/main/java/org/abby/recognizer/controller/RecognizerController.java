b0a94529d7c03bf786aea2ace7c02eb100338f9d
t-marxi <igor.klimovskiy@gmail.com>
2018-11-01 15:46:17 +0300

    deleted warning

--- src/main/java/org/abby/recognizer/controller/RecognizerController.java
+++ src/main/java/org/abby/recognizer/controller/RecognizerController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.abby.recognizer.data.ResponseData;
 import org.abby.recognizer.service.RecognizeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
